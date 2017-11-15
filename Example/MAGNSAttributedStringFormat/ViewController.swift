c03d776ebb159e11beb6ee4451b45082faaaadd3
Nikita Kazakov <kazakov@magora-systems.com>
2017-11-15 08:40:53 +0700

    Test extension
    Tested changes. There all will be changes.
    Redmine. Refs: #80373

--- Example/MAGNSAttributedStringFormat/ViewController.swift
+++ Example/MAGNSAttributedStringFormat/ViewController.swift
@@ -7,17 +7,39 @@
 //
 
 import UIKit
+import MAGNSAttributedStringFormat
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.testAttributedFormatString()
     }
+    
+    
+    func testAttributedFormatString() {
+        let blue = NSMutableAttributedString(string: "blue", attributes: [
+            NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 13),
+            NSAttributedStringKey.foregroundColor: UIColor.blue
+            ])
+        
+        let green = NSMutableAttributedString(string: "green", attributes: [
+            NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 13),
+            NSAttributedStringKey.foregroundColor: UIColor.green
+            ])
+        
+        let red = NSMutableAttributedString(string: "red", attributes: [
+            NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 13),
+            NSAttributedStringKey.foregroundColor: UIColor.red
+            ])
+        
+        let attr = NSAttributedString().attributedStringWith(format: "My favorite colors is %@ and %@, but not %@", arguments: red, blue, green)
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        self.descriptionLabel.attributedText = attr
     }
 
 }
