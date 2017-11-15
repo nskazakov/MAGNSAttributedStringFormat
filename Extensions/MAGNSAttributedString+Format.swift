c5e7767dc98ba67edce09bc9c464d2c101805487
Nikita Kazakov <kazakov@magora-systems.com>
2017-11-15 08:32:50 +0700

    Change extension NSAttributedString.
    Implemented extension that allows you to receive an attributed string array.
    Redmine. Refs: #80373

--- Extensions/MAGNSAttributedString+Format.swift
+++ Extensions/MAGNSAttributedString+Format.swift
@@ -2,17 +2,72 @@ import Foundation
 
 extension NSAttributedString {
     
-    private func initWith(attributes: NSDictionary, format: String, arguments: va_list) -> NSAttributedString {
+    public func attributedStringWith(format: NSString, arguments: NSMutableAttributedString...) -> NSAttributedString {
+        let result = self.initWith(format: format, arguments: arguments)
+        return result
+    }
+    
+    public func attributedStringWith(attributes: [NSAttributedStringKey : Any], format: NSString, arguments: NSMutableAttributedString...) -> NSAttributedString {
+        let result = self.initWith(attributes: attributes, format: format as String, arguments: arguments)
+        return result
+    }
+    
+    private func initWith(format: NSString, arguments: [NSMutableAttributedString]) -> NSAttributedString {
+        let result = self.initWith(attributes: nil, format: format as String, arguments: arguments)
+        return result
+    }
+    
+    private func initWith(attributes: [NSAttributedStringKey : Any]?, format: String, arguments: [NSMutableAttributedString]) -> NSAttributedString {
         var attributedString = NSMutableAttributedString()
-        attributedString = NSMutableAttributedString.init(string: format, attributes: attributes as? [NSAttributedStringKey : Any])
+        if let attribute = attributes {
+            attributedString = NSMutableAttributedString(string: format, attributes: attribute)
+        } else {
+            attributedString = NSMutableAttributedString(string: format)
+        }
+        
         attributedString.beginEditing()
         
-        var maxPosition = 0
-        var parseResult: FormatParseResult!
-        parseResult.formatStringParser(format: format as NSString, maxPosition: maxPosition)
+        let maxPosition = arguments.count
+        let parseResult = FormatStringParses()
+        let parseResults = parseResult.formatStringParser(format: format as NSString, maxPosition: maxPosition)
+            .sorted(by: { (lhs, rhs) -> Bool in
+                return lhs.range.location < rhs.range.location
+            })
         
+        var index = 0
+        var attributeStringArray = [NSMutableAttributedString]()
         
+        for _ in index..<maxPosition {
+            index += 1
+            for argument in arguments {
+                attributeStringArray.append(argument)
+            }
+        }
+        
+        for result in parseResults {
+            var arg = attributeStringArray[result.index]
+            
+            if arg.isKind(of: NSAttributedString.superclass()!) {
+                if let attribute = attributes {
+                    let argCopy: NSMutableAttributedString = arg.mutableCopy() as! NSMutableAttributedString
+                    argCopy.setAttributes(attribute, range: NSMakeRange(0, argCopy.length))
+                    
+                    arg.enumerateAttributes(in: NSMakeRange(0, argCopy.length),
+                                            options: .reverse,
+                                            using: { (attr, range, stop) in
+                                                argCopy.addAttributes(attr, range: range)
+                    })
+                    arg = argCopy
+                    
+                }
+                
+                attributedString.replaceCharacters(in: parseResult.range, with: arg)
+            } else {
+                attributedString.replaceCharacters(in: parseResult.range, with: arg.description)
+            }
+        }
+        attributedString.endEditing()
 
-        return NSAttributedString.init(attributedString: attributedString)
+        return NSMutableAttributedString(attributedString: attributedString)
     }
 }
